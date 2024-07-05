e75cab6beb12cd155481ff78b858cb9557806955
Jaxon Adams <96997462+JaxonAdams@users.noreply.github.com>
2024-07-05 15:42:56 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,31 @@
+// create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// create comments array
+let comments = [];
+
+// get comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+// add comment
+app.post('/comments', (req, res) => {
+    comments.push(req.body);
+    res.json(req.body);
+});
+
+// delete comment
+app.delete('/comments/:id', (req, res) => {
+    comments = comments.filter((comment, index) => index !== parseInt(req.params.id));
+    res.json(comments);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
